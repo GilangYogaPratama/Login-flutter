e7e7664c13115e1adf49c3e319e37953556b674e
Gilang Yoga <gilangy538@gmail.com>
2023-05-18 13:01:49 +0700

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:login_app_flutter/home.dart';
-import 'package:login_app_flutter/login.dart';
+import 'package:salam_app/home.dart';
+import 'package:salam_app/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
